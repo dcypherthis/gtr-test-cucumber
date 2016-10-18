43b07f94a6d9dff0d380d43387f332dbb34f1fa9
Josh Cypher <jcypher@myemma.com>
2016-10-18 10:41:44 -0500

    :wrench: add wdio configs

---
+++ config/local/dev.conf.js
@@ -0,0 +1,24 @@
+require('babel-core/register');
+const merge = require('deepmerge');
+const masterConf = require('../master.conf.js');
+const devConf = {
+    specs: [
+        './devFeatures/**/*.feature',
+    ],
+    capabilities: [
+        // {
+        //    browserName: 'firefox',
+        //    autoAcceptAlerts: true,
+        //    maxInstances: '1',
+        // },
+        {
+            browserName: 'chrome',
+            autoAcceptAlerts: true,
+            // version: '50',
+            maxInstances: '1',
+        },
+    ],
+    logLevel: 'result',
+    reporters: ['dot', 'spec'],
+};
+exports.config = merge(masterConf.config, devConf);
