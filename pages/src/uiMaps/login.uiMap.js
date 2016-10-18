d988086beabfa9ee8ca970ba293d1a7d84cec0c4
Josh Cypher <jcypher@myemma.com>
2016-10-18 13:43:09 -0500

    :moneybag: add base uiMaps

---
+++ pages/src/uiMaps/login.uiMap.js
@@ -0,0 +1,42 @@
+/**
+ * A class with keys and values for css selectors
+ */
+export default class UiMap {
+
+    /** URL & Title **/
+    get url() {
+        return `${browser.options.env.login_url}`;
+    }
+
+    get title() {
+        return 'head > title';
+    }
+
+    get titleText() {
+        return 'AdonisJs - Node.Js MVC Framework';
+    }
+
+    /** Navigation **/
+    get navHeading() {
+        return 'body > div.hero.is-primary > nav > h1 > a';
+    }
+
+    get aNavSongs() {
+        return '.show-info';
+    }
+
+    get aNavSetlists() {
+        return '.close-help';
+    }
+
+    get aNavProfile() {
+        return '#help > div > article > p';
+    }
+
+    get aNavLogout() {
+        return '#help';
+    }
+
+    /** Page Contents**/
+
+}
